975637ae1df8600a0aba7e5ec6655400f2b1315d
Zhao Yu <gil@MacBook-Pro.local>
2020-09-12 21:39:31 +0800

    fixed style

--- packages/app-5times-todoist/src/App.js
+++ packages/app-5times-todoist/src/App.js
@@ -48,10 +48,15 @@ function App() {
 							<span>({dayjs(showDay).format('ddd')})</span>
 						</>
 					)}
-					<span className="btn" onClick={() => {
-						setShowDay(today)
-						setWeekList(getWeekList(today))
-					}}>Today</span>
+					<span
+						className="btn"
+						onClick={() => {
+							setShowDay(today);
+							setWeekList(getWeekList(today));
+						}}
+					>
+						Today
+					</span>
 				</h3>
 				<div className="todo-list-wrap">
 					<Algorithm day={showDay} today={today} />
@@ -65,20 +70,35 @@ function App() {
 					getWeekList={getWeekList}
 				/>
 				<div className="info">
-					刷题狂魔: <a href="https://jinshuju.net/f/BQXggl/">每日一题打卡表</a>
+					刷题狂魔：<a href="https://jinshuju.net/f/BQXggl/">每日一题打卡表</a>
 					<ol>
 						<li>
-							读题 + 思考 （如果基础薄弱 可以给自己10分钟 最多15分钟）这里不能略过 有思路 直接做 直接写 ||
-							超时 ——&gt;&gt; 直接看解法！注意！多解法，比较解法优劣（理解学习和运用算法 不是让你去造轮子）
-							背诵、默写这样好的写法（并不是死记硬背即可，先背诵记住了之后，一般来说肯定就能理解了。很多题目以后看到就条件反射了）（不能打击自己的积极性）
+							<p>读题 + 思考 （基础薄弱 10~15分钟）不能略过!!</p>
+							<p>有思路: 直接做，直接写</p>
+							<p>
+								超时 ——&gt;&gt; 直接看解法！注意！多解法，比较解法优劣（理解学习和运用算法
+								不是让你去造轮子）
+							</p>
+							<p>
+								背诵、默写好的写法（不是死记硬背，先背诵记住了之后，一般来说肯定就能理解了。很多题目以后看到就条件反射了）（不能打击自己的积极性）
+							</p>
 						</li>
 						<li>
-							马上自己写（一开始可能会有bug 没关系 debug debug debug 修改修改修改） ——&gt; LeetCode提交
-							多种写法、体会 ——&gt; 优化！(最重要的是执行时间）
+							<p>
+								马上自己写（一开始可能会有bug 没关系 debug debug debug 修改修改修改） ——&gt;
+								LeetCode提交
+							</p>
+							<p>多种写法、体会 ——&gt; 优化！（最重要的是执行时间）</p>
 						</li>
-						<li>过了24 小时的时间以后，再次重复做题 不同解法的熟练程度 ——&gt; 专项练习</li>
-						<li>过了一周之后: 反复回来练习相同的题目</li>
-						<li>面试前一周恢复性的训练</li>
+						<li>
+							<p>过了24 小时的时间以后，再次重复做题 不同解法的熟练程度 ——&gt; 专项练习</p>
+						</li>
+						<li>
+							<p>过了一周之后: 反复回来练习相同的题目</p>
+						</li>
+						<li>
+							<p>面试前一周恢复性的训练</p>
+						</li>
 					</ol>
 				</div>
 			</div>
